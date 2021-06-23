25ed01945fc70c0b6075122fbfe6d7b2ffee9997
ctanguy2020 <ctanguy2020@102SE13-84J3P02.ad.campus-eni.fr>
2021-06-23 13:20:39 +0200

    ajout eception

--- src/fr/eni/projetEnchere/bll/ArticleVenduManager.java
+++ src/fr/eni/projetEnchere/bll/ArticleVenduManager.java
@@ -1,9 +1,9 @@
 package fr.eni.projetEnchere.bll;
 
-
 import java.sql.Date;
 import java.time.LocalDate;
 
+import fr.eni.projetEnchere.BusinessException;
 import fr.eni.projetEnchere.bo.boArticleVendu;
 import fr.eni.projetEnchere.dal.ArticleVenduDAO;
 import fr.eni.projetEnchere.dal.jdbcTools.DAOFactory;
@@ -29,23 +29,32 @@ public class ArticleVenduManager {
 
 	public void ajoutArticle(
 
-			String nomArticle,
-			String description,
-			LocalDate dateDebutEnchere,
-			LocalDate finDebutEnchere,
-			int prixInitial,
-			int prixVente,
-			int idUtilisateur,
-			int categorie)
-			 {
+			String nomArticle, String description, LocalDate dateDebutEnchere, LocalDate finDebutEnchere,
+			int prixInitial, int prixVente, int idUtilisateur, int categorie) {
 
 		// Vérification des données provenant du formulaire
 
 		// Après vérification, création de l'article
-		boArticleVendu nvlArticle = new boArticleVendu(nomArticle, description, dateDebutEnchere,
-				finDebutEnchere, prixInitial, prixVente, idUtilisateur, categorie);
+		boArticleVendu nvlArticle = new boArticleVendu(nomArticle, description, dateDebutEnchere, finDebutEnchere,
+				prixInitial, prixVente, idUtilisateur, categorie);
 
 		// Ajout à la BDD
 		articleVendu.insert(nvlArticle);
+	}
+
+	/*******************************************************************************
+	 *
+	 * ENSEMBLE DES MÉTHODES INDÉPENDANTES DE VERIFICATION DE CHAMPS
+	 *
+	 *******************************************************************************/
+
+	private void validerArticleVendu(boArticleVendu articleVendu, BusinessException businessException) {
+
+	}
+	
+	private void validerNomArticle(boArticleVendu articleVendu, BusinessException businessException) {
+		if(articleVendu.getNomArticle().length() > 5 && articleVendu.getNomArticle().length() < 30) {
+			
+		}
 	}
 }
