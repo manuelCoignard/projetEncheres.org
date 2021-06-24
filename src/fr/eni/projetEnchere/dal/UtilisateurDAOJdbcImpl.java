48b443cac71ab4d234f7837cfeeab8b29f977c27
Coignard Manuel <manuel.coignard@gmail.com>
2021-06-24 14:22:47 +0200

    UPDATE mise à jour de la gestion d'erreur à la connexion

--- src/fr/eni/projetEnchere/dal/UtilisateurDAOJdbcImpl.java
+++ src/fr/eni/projetEnchere/dal/UtilisateurDAOJdbcImpl.java
@@ -67,7 +67,6 @@ public class UtilisateurDAOJdbcImpl implements UtilisateurDAO{
 	 */
 	@Override
 	public boUtilisateur connectionEmail(String email, String mdp) throws BusinessException {
-		//TODO créer la variable BusinessException dès le début de la méthode
 		
 		boUtilisateur utilisateur = null;
 		
@@ -100,7 +99,8 @@ public class UtilisateurDAOJdbcImpl implements UtilisateurDAO{
 				//Vérfication de la concordance des mots de passe
 				if(!rs.getString("mot_de_passe").equals(mdp)) {
 					BusinessException be = new BusinessException();
-					//TODO ajout du message d'erreur
+					be.ajouterErreur(CodesErreursDAL.WRONG_PASSWORD);
+					throw be;
 				}
 				
 				//Récupération des données de l'utilisateur 
@@ -115,13 +115,14 @@ public class UtilisateurDAOJdbcImpl implements UtilisateurDAO{
 				int credit = rs.getInt("credit");
 				boolean admin = rs.getBoolean("administrateur");
 					
+				//Construction de l'utilisateur
 				utilisateur =new boUtilisateur(noId, pseudo, nom, prenom, email, tel, adresse, cp, ville, mdp, credit, admin);
 		}
 		catch(SQLException e)
 		{
 			e.printStackTrace();
 			BusinessException be = new BusinessException();
-			be.ajouterErreur(CodesErreursDAL.SELECT_INVALID_USER);
+			be.ajouterErreur(CodesErreursDAL.SELECT_USER_ERROR);
 			throw be;
 		}
 
